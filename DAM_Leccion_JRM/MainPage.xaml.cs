8f391f401251dd7dd61c69d42679e4964002b61e
Jonathan <21030544@itesa.edu.mx>
2025-01-28 18:52:38 -0600

    Version_02: Instrucciones basicas en botón y caja de texto.

--- DAM_Leccion_JRM/MainPage.xaml.cs
+++ DAM_Leccion_JRM/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_JRM
+﻿using DAM_Leccion_JRM.ViewModel;
+
+namespace DAM_Leccion_JRM
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,19 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void ejecutar() {
+            PersonaModel personaModel = new PersonaModel();
+            personaModel.Nombre = "Hola estoy aqui";
+
+            txtNombre.Text = personaModel.Nombre;
+            //txtNombre.Text = "Hola estoy aqui";   
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema","Se ha guardado el registro en la DB","Aceptar");
         }
     }
 
